d0cc428736f69a15419065db9cf58f4d539c715a
show-with-ta <sukiyakids@gmil.com>
2020-07-06 10:59:57 +0900

    chat-space-add-ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Main__Chat__box">
+          <div class="Main__Chat__box__data">
+            <div class="Main__Chat__box__data__name">
+              ${message.user_name}
+            </div>
+            <div class="Main__Chat__box__data__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Main__Chat__box__message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Main__Chat__box__message__content" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Main__Chat__box">
+        <div class="Main__Chat__box__data">
+          <div class="Main__Chat__box__data__name">
+            ${message.user_name}
+          </div>
+          <div class="Main__Chat__box__data__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Main__Chat__box__message">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Main__Footer__form__content').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",  
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Main__Chat').append(html);
+      $('.Main__Chat').animate({ scrollTop: $('.Main__Chat')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+    return false
+  });
+});
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module ChatSpace
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -21,6 +20,7 @@ module ChatSpace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
